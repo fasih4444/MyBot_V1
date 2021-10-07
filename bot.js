f6c3534713f680f48ba75552223eaa83236a09b6
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-10-07 15:05:10 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -374,7 +374,7 @@ async function whatsAsena () {
                         command.pattern.test(text_msg))))) {
 
                     let sendMsg = false;
-                    var chat = conn.chats.get(msg.key.remoteJid)
+                    var chat = DrkBotCN.chats.get(msg.key.remoteJid)
                         
                     if ((config.SUDO !== false && msg.key.fromMe === false && command.fromMe === true &&
                         (msg.participant && config.SUDO.includes(',') ? config.SUDO.split(',').includes(msg.participant.split('@')[0]) : msg.participant.split('@')[0] == config.SUDO || config.SUDO.includes(',') ? config.SUDO.split(',').includes(msg.key.remoteJid.split('@')[0]) : msg.key.remoteJid.split('@')[0] == config.SUDO)
