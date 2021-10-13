0bc451b04f26f6d98282f356e2bb021da65f3983
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-10-13 09:57:18 -0500

    Update ia-resp.js

--- plugins/ia-resp.js
+++ plugins/ia-resp.js
@@ -15,7 +15,7 @@ const Lang = Language.getString('simi');
 if (Config.WORKTYPE == 'private') {
   
 	DrkBox.addCommand({pattern: 'simi ?(.*)', fromMe: true, desc: Lang.DESC}, async (message, match) => {
-                if (match[1] === '') return await message.reply(Lang.what, {quoted: message.data});
+                if (match[1] === '') await message.sendMessage(Lang.what, MessageType,text, {quoted: message.data});
 		if (match[1].includes('xxx') || match[1].includes('porno')) return await message.reply(Lang.err_type, {quoted: message.data});
 		const url = `https://api.simsimi.net/v2/?text=${match[1]}&lc=es&cf=true`;
 		try {
@@ -30,7 +30,7 @@ if (Config.WORKTYPE == 'private') {
 else if (Config.WORKTYPE == 'public') {
 
 	DrkBox.addCommand({pattern: 'simi ?(.*)', fromMe: false, desc: Lang.DESC}, async (message, match) => {
-                if (match[1] === '') return await message.reply(Lang.what, {quoted: message.data});
+                if (match[1] === '') await message.sendMessage(Lang.what, MessageType,text, {quoted: message.data});
 		if (match[1].includes('xxx') || match[1].includes('porno')) return await message.reply(Lang.err_type, {quoted: message.data});
 		const url = `https://api.simsimi.net/v2/?text=${match[1]}&lc=es&cf=true`;
 		try {
