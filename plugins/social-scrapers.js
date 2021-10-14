1ee7a03f67cc82e2b3b6f321432d069a56233f41
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-10-14 17:47:33 -0500

    Update social-scrapers.js

--- plugins/social-scrapers.js
+++ plugins/social-scrapers.js
@@ -75,15 +75,15 @@ else if (Config.WORKTYPE == 'public') {
             await message.sendMessage(errorMessage(Lang.iErr))
         })
     });
-/*
-DrkBox.addCommand({pattern: 'igdown ?(.*)', fromMe: true}, (async (message, match) => {
-    var igdesc = await dbot.igdl(`match[1]`)
-    var buffer_data = await axios.get(igdesc.url, { responseType: 'arraybuffer'})
-    await message.sendMessage(Buffer.from(buffer_data.medias), MessageType.image, { mimetype: Mimetype.png, caption: `${MLang.by}` })
-}));
-*/
 
-    
+DrkBox.addCommand({pattern: 'igdown ?(.*)', fromMe: true}, (async (message, match) => {
+    var igdesc = await dbot.igdl(match[1])
+    var buffer_data = await axios.get(igdesc.url, { responseType: 'arraybuffer'})
+    await message.sendMessage(Buffer.from(buffer_data.data.medias), MessageType.image, { mimetype: Mimetype.png, caption: `${MLang.by}` })
+}));
+
+
+    /*
     DrkBox.addCommand({ pattern: 'igdown ?(.*)', fromMe: true, desc: Lang.DESC}, async (message, match) => {
 	 const link = match[1]
 	 const buffer = await dbot.igdl(`${link}`)
@@ -94,5 +94,5 @@ DrkBox.addCommand({pattern: 'igdown ?(.*)', fromMe: true}, (async (message, matc
                  await message.sendMessage(Buffer.from(buffer.url), MessageType.image)
              }
          }
-    });
+    });*/
 }
