5a464a2fb93486440dfb61f6f42e0b8057fb56d7
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-10-17 14:57:20 -0500

    Update social-scrapers.js

--- plugins/social-scrapers.js
+++ plugins/social-scrapers.js
@@ -21,56 +21,39 @@ const MLang = Language.getString('messages');
 
 let wk = Config.WORKTYPE == 'public' ? false : true
     
-    DrkBox.addCommand({ pattern: 'insta ?(.*)', fromMe: wk, desc: Lang.DESC}, async (message, match) => {
+DrkBox.addCommand({ pattern: 'insta ?(.*)', fromMe: wk, desc: Lang.DESC}, async (message, match) => {
+    const userName = match[1]
+    if (userName === '') return await message.sendMessage(errorMessage(Lang.need))
+    await message.sendMessage(infoMessage(Lang.loading))
+    await axios.get(`https://api.zeks.xyz/api/igstalk?apikey=${KLang.KZ}&username=${userName}`).then(async (response) => {
+        const { username, fullname, follower, following, profile_pic } = response.data
+        const instascrap = await axios.get(profile_pic, { responseType: 'arraybuffer' })
+        const msg = `*Nombre:* ${fullname}\n*Usuario:* ${username}\n*Seguidores:* ${follower}\n*Siguiendo:* ${following}`
+        await message.sendMessage(Buffer.from(instascrap.data), MessageType.image, { caption: msg })
+    }).catch(async (err) => {
+        await message.sendMessage(errorMessage(Lang.iErr))
+    })
+});
 
-        const userName = match[1]
-        if (userName === '') return await message.sendMessage(errorMessage(Lang.need))
-
-        await message.sendMessage(infoMessage(Lang.loading))
-
-        await axios.get(`https://api.zeks.xyz/api/igstalk?apikey=${KLang.KZ}&username=${userName}`).then(async (response) => {
-            const { username, fullname, follower, following, profile_pic } = response.data
-            const instascrap = await axios.get(profile_pic, { responseType: 'arraybuffer' })
-            const msg = `*Nombre:* ${fullname}\n*Usuario:* ${username}\n*Seguidores:* ${follower}\n*Siguiendo:* ${following}`
-            await message.sendMessage(Buffer.from(instascrap.data), MessageType.image, { caption: msg })
-        }).catch(async (err) => {
-            await message.sendMessage(errorMessage(Lang.iErr))
-        })
-    });
-
-DrkBox.addCommand({ pattern: 'twt ?(.*)', fromMe: wk,  dontAddCommandList: true, desc: "Descargas de twitter" }, async (message, match) => {
+DrkBox.addCommand({ pattern: 'twt ?(.*)', fromMe: wk, dontAddCommandList: true, desc: "Descargas de twitter" }, async (message, match) => {
     const twtLink = match[1]
-
     if (!twtLink) return await message.sendMessage(errorMessage("🤖 Necesito un link!"))
-
     await message.sendMessage(infoMessage("🤖 Haciendo Busqueda"))
 
     await axios.get(`https://api-anoncybfakeplayer.herokuapp.com/twdown?url=${twtLink}`).then(async (response) => {
         const {format, result,} = response.data
         const profileBuffer = await axios.get(result, {responseType: 'arraybuffer'})
         const msg = `${format}`
+    if (msg === 'Image/jpg or png') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {caption: msg,})}
+    if (msg === 'video/mp4') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {caption: msg,})}
 
-      if (msg === 'Image/jpg or png') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {caption: msg,})}
-      if (msg === 'video/mp4') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {caption: msg,})}
-    }).catch(async (err) => { await message.sendMessage(errorMessage("🤖 Parece que hay un error!0" ))
-}));
+    }).catch (async (err) => {
+     await message.sendMessage(errorMessage("🤖 Parece que hay un error!0" ))
+    });
+});
 
 DrkBox.addCommand({pattern: 'igdown ?(.*)', fromMe: wk}, (async (message, match) => {
     var igdesc = await dbot.igdl(match[1])
     var buffer_data = await axios.get(igdesc.url, { responseType: 'arraybuffer'})
     await message.sendMessage(Buffer.from(buffer_data.data), MessageType.image, { mimetype: Mimetype.png, caption: `${MLang.by}` })
 }));
-
-
-    /*
-    DrkBox.addCommand({ pattern: 'igdown ?(.*)', fromMe: wk, desc: Lang.DESC}, async (message, match) => {
-	 const link = match[1]
-	 const buffer = await dbot.igdl(`${link}`)
-         for (let i of buffer ? buffer : buffer.medias){
-             if(i.url.includes('.mp4')){
-                 await message.sendMessage(Buffer.from(buffer.url), MessageType.video)
-             } else {
-                 await message.sendMessage(Buffer.from(buffer.url), MessageType.image)
-             }
-         }
-    });*/
