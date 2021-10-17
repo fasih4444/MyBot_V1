ee0acc6b53334e0e9ff367cf7639fde274b10e70
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-10-17 18:48:41 -0500

    Update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -17,154 +17,77 @@ const aniN = "¡waifu o neko!"
 const Language = require('../language');
 const MLang = Language.getString('messages');
 
-if (Config.WORKTYPE == 'private') {
+let wk = Config.WORKTYPE == 'public' ? false : true
 
-	DrkBox.addCommand({pattern: 'infoanime', fromMe: true, desc: H_DESC}, (async (message, match) => {
-		await message.sendMessage('🔞 *TODO ANIME* 🔞\n\n➡️ /sfw\n\n➡️ /nsfw');
-	}));
+DrkBox.addCommand({pattern: 'infoanime', fromMe: wk, desc: H_DESC}, (async (message, match) => {
+	await message.sendMessage('🔞 *TODO ANIME* 🔞\n\n➡️ /sfw\n\n➡️ /nsfw');
+}));
 
-	DrkBox.addCommand({pattern: 'calendar', fromMe: true}, (async (message, match) => {
-		var image = await axios.get ('https://raw.githubusercontent.com/BotPrivateDrk/WhatsAsenaDuplicated/master/media/gif/calender.jpg', {responseType: 'arraybuffer'})
-		await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.jpg, quoted: message.data, caption: `${MLang.by}` })
-	}));
-	
-	DrkBox.addCommand({pattern: "sfw ?(.*)", fromMe: true}, (async (message, match) => {
-		if (match[1] === "") {
-			try{
-				await message.sendMessage(infoMessage(aniN))
-			}catch (err) {
-				await message.reply(`Error:\n${Ierr}`, MessageType.text)
-			 }
-		}
-		else if (match[1] === "waifu") {
-			try{
-				await axios.get(`https://api.waifu.pics/sfw/waifu`).then(async (response) => {
-					const {url} = response.data
-					const amin = await axios.get(url, { responseType: 'arraybuffer' })
-					await message.sendMessage(Buffer.from(amin.data), MessageType.image, { caption: `${MLang.by}` })
-				});
-			}catch (err) {
-				await message.reply(`*Error:*\n${Ierr}`, MessageType.text)
-			 }
-		}
-		else if (match[1] === "neko") {
-			try{
-				await axios.get(`https://api.waifu.pics/sfw/neko`).then(async (response) => {
-					const {url} = response.data
-					const amin = await axios.get(url, { responseType: 'arraybuffer' })
-					await message.sendMessage(Buffer.from(amin.data), MessageType.image, { caption: `${MLang.by}` })
-				});
-			}catch (err) {
-				await message.reply(`*Error:*\n${Ierr}`, MessageType.text)
-			 }
-    	}
-	}));
-	
-	DrkBox.addCommand({pattern: "nsfw ?(.*)", fromMe: true, desc: H_DESC}, (async (message, match) => {
-		if (match[1] === "") {
-			try{
-				await message.sendMessage(infoMessage(aniN))
-			}catch (err) {
-				await message.reply(`Error:\n${Ierr}`, MessageType.text)
-			 }
-		}
-		else if (match[1] === "waifu") {
-			try{
-				await axios.get(`https://api.waifu.pics/nsfw/waifu`).then(async (response) => {
-					const {url} = response.data
-					const amin = await axios.get(url, { responseType: 'arraybuffer' })
-					await message.sendMessage(Buffer.from(amin.data), MessageType.image, { caption: `${MLang.by}` })
-				});
-			}catch (err) {
-				await message.reply(`*Error:*\n${Ierr}`, MessageType.text)
-			 }
-		}
-		else if (match[1] === "neko") {
-			try{
-				await axios.get(`https://api.waifu.pics/nsfw/neko`).then(async (response) => {
-					const {url} = response.data
-					const amin = await axios.get(url, { responseType: 'arraybuffer' })
-					await message.sendMessage(Buffer.from(amin.data), MessageType.image, { caption: `${MLang.by}` })
-				});
-			}catch (err) {
-				await message.reply(`*Error:*\n${Ierr}`, MessageType.text)
-			 }
-		}
-	}));
-}
+DrkBox.addCommand({pattern: 'calendar', fromMe: wk}, (async (message, match) => {
+    var image = await axios.get ('https://raw.githubusercontent.com/BotPrivateDrk/WhatsAsenaDuplicated/master/media/gif/calender.jpg', {responseType: 'arraybuffer'})
+    await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.jpg, quoted: message.data, caption: `${MLang.by}` })
+}));
 
-else if (Config.WORKTYPE == 'public') {
-	
-DrkBox.addCommand({pattern: 'infoanime', fromMe: false, desc: H_DESC}, (async (message, match) => {
-		await message.sendMessage('🔞 *TODO ANIME* 🔞\n\n➡️ /sfw\n\n➡️ /nsfw');
-	}));
+DrkBox.addCommand({pattern: "sfw ?(.*)", fromMe: wk}, (async (message, match) => {
+    if (match[1] === "") {
+        try{
+            await message.sendMessage(infoMessage(aniN))
+        }catch (err) {
+            await message.reply(`Error:\n${Ierr}`, MessageType.text)
+         }
+    }
+    else if (match[1] === "waifu") {
+        try{
+            await axios.get(`https://api.waifu.pics/sfw/waifu`).then(async (response) => {
+                const {url} = response.data
+                const amin = await axios.get(url, { responseType: 'arraybuffer' })
+                await message.sendMessage(Buffer.from(amin.data), MessageType.image, { caption: `${MLang.by}` })
+            });
+        }catch (err) {
+            await message.reply(`*Error:*\n${Ierr}`, MessageType.text)
+         }
+    }
+    else if (match[1] === "neko") {
+        try{
+            await axios.get(`https://api.waifu.pics/sfw/neko`).then(async (response) => {
+                const {url} = response.data
+                const amin = await axios.get(url, { responseType: 'arraybuffer' })
+                await message.sendMessage(Buffer.from(amin.data), MessageType.image, { caption: `${MLang.by}` })
+            });
+        }catch (err) {
+            await message.reply(`*Error:*\n${Ierr}`, MessageType.text)
+         }
+    }
+}));
 
-	DrkBox.addCommand({pattern: 'calendar', fromMe: false}, (async (message, match) => {
-		var image = await axios.get ('https://raw.githubusercontent.com/BotPrivateDrk/WhatsAsenaDuplicated/master/media/gif/calender.jpg', {responseType: 'arraybuffer'})
-		await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.jpg, quoted: message.data, caption: `${MLang.by}` })
-	}));
-	
-	DrkBox.addCommand({pattern: "sfw ?(.*)", fromMe: false}, (async (message, match) => {
-		if (match[1] === "") {
-			try{
-				await message.sendMessage(infoMessage(aniN))
-			}catch (err) {
-				await message.reply(`Error:\n${Ierr}`, MessageType.text)
-			 }
-		}
-		else if (match[1] === "waifu") {
-			try{
-				await axios.get(`https://api.waifu.pics/sfw/waifu`).then(async (response) => {
-					const {url} = response.data
-					const amin = await axios.get(url, { responseType: 'arraybuffer' })
-					await message.sendMessage(Buffer.from(amin.data), MessageType.image, { caption: `${MLang.by}` })
-				});
-			}catch (err) {
-				await message.reply(`*Error:*\n${Ierr}`, MessageType.text)
-			 }
-		}
-		else if (match[1] === "neko") {
-			try{
-				await axios.get(`https://api.waifu.pics/sfw/neko`).then(async (response) => {
-					const {url} = response.data
-					const amin = await axios.get(url, { responseType: 'arraybuffer' })
-					await message.sendMessage(Buffer.from(amin.data), MessageType.image, { caption: `${MLang.by}` })
-				});
-			}catch (err) {
-				await message.reply(`*Error:*\n${Ierr}`, MessageType.text)
-			 }
-    	}
-	}));
-	
-	DrkBox.addCommand({pattern: "nsfw ?(.*)", fromMe: false, desc: H_DESC}, (async (message, match) => {
-		if (match[1] === "") {
-			try{
-				await message.sendMessage(infoMessage(aniN))
-			}catch (err) {
-				await message.reply(`Error:\n${Ierr}`, MessageType.text)
-			 }
-		}
-		else if (match[1] === "waifu") {
-			try{
-				await axios.get(`https://api.waifu.pics/nsfw/waifu`).then(async (response) => {
-					const {url} = response.data
-					const amin = await axios.get(url, { responseType: 'arraybuffer' })
-					await message.sendMessage(Buffer.from(amin.data), MessageType.image, { caption: `${MLang.by}` })
-				});
-			}catch (err) {
-				await message.reply(`*Error:*\n${Ierr}`, MessageType.text)
-			 }
-		}
-		else if (match[1] === "neko") {
-			try{
-				await axios.get(`https://api.waifu.pics/nsfw/neko`).then(async (response) => {
-					const {url} = response.data
-					const amin = await axios.get(url, { responseType: 'arraybuffer' })
-					await message.sendMessage(Buffer.from(amin.data), MessageType.image, { caption: `${MLang.by}` })
-				});
-			}catch (err) {
-				await message.reply(`*Error:*\n${Ierr}`, MessageType.text)
-			 }
-		}
-	}));
-}
+DrkBox.addCommand({pattern: "nsfw ?(.*)", fromMe: wk, desc: H_DESC}, (async (message, match) => {
+    if (match[1] === "") {
+        try{
+            await message.sendMessage(infoMessage(aniN))
+        }catch (err) {
+            await message.reply(`Error:\n${Ierr}`, MessageType.text)
+         }
+    }
+    else if (match[1] === "waifu") {
+        try{
+            await axios.get(`https://api.waifu.pics/nsfw/waifu`).then(async (response) => {
+                const {url} = response.data
+                const amin = await axios.get(url, { responseType: 'arraybuffer' })
+                await message.sendMessage(Buffer.from(amin.data), MessageType.image, { caption: `${MLang.by}` })
+            });
+        }catch (err) {
+            await message.reply(`*Error:*\n${Ierr}`, MessageType.text)
+         }
+    }
+    else if (match[1] === "neko") {
+        try{
+            await axios.get(`https://api.waifu.pics/nsfw/neko`).then(async (response) => {
+                const {url} = response.data
+                const amin = await axios.get(url, { responseType: 'arraybuffer' })
+                await message.sendMessage(Buffer.from(amin.data), MessageType.image, { caption: `${MLang.by}` })
+            });
+        }catch (err) {
+            await message.reply(`*Error:*\n${Ierr}`, MessageType.text)
+         }
+    }
+}));
