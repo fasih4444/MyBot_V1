1933c7bd44601040d56eb1bf0728943cff36ace8
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-10-22 01:34:23 -0500

    Update botton.js

--- plugins/botton.js
+++ plugins/botton.js
@@ -42,8 +42,24 @@ DrkBox.addCommand({on: 'text', fromMe: wk}, (async (message, match) => {
 	} else if (message.message.includes('OWNER')) {
 		await message.client.sendMessage(message.jid, {displayname: "Ian", vcard: vCard}, MessageType.contact);
 	}
-	else if (message.message.client.includes('UPDATE')) {
+	else if (message.client.includes('UPDATE')) {
 		await message.client.sendMessage(message.jid, dapdate, MessageType.text);
+	} else if (message.message.includes('dbot1')) {
+		const rows = [
+		 {title: 'Row 1', description: "Hello it's description 1", rowId:"rowid1"},
+		 {title: 'Row 2', description: "Hello it's description 2", rowId:"rowid2"}
+		]
+
+		const sections = [{title: "Section 1", rows: rows}]
+
+		const button = {
+		 buttonText: 'Click Me!',
+		 description: "Hello it's list message",
+	 	 sections: sections,
+	 	 listType: 1
+		}
+
+		await message.client.sendMessage(message.jid, button, MessageType.listMessage)
 	}
 }));
 /*################# FIN #################*/
