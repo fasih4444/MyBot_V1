97f8ff6f054381be599eea5bb909601bec6540df
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-10-24 01:58:39 -0500

    Update social-scrapers.js

--- plugins/social-scrapers.js
+++ plugins/social-scrapers.js
@@ -23,26 +23,25 @@ const instagram = async (url, key) => {
     const _0x4a94a8 = _0x185a; function _0x3f3b() { const _0x37037c = ['drk', 'htt', 'ps:', 'nti', 'ouy', 'aw.', 'kit', '/in', 'sta', '?ur', 'dat', 'get', 'arr']; _0x3f3b = function () { return _0x37037c; }; return _0x3f3b(); } function _0x185a(_0x38e93d, _0x3f3b83) { const _0x185a5f = _0x3f3b(); _0x185a = function (_0x829ec5, _0x405d60) { _0x829ec5 = _0x829ec5 - 0xe5; let _0x20f676 = _0x185a5f[_0x829ec5]; return _0x20f676; }; return _0x185a(_0x38e93d, _0x3f3b83); } if (key != _0x4a94a8(0xe5) + 'bot') throw new Error('Drk' + 'bot'); const response = await axios(_0x4a94a8(0xe6) + _0x4a94a8(0xe7) + '//u' + _0x4a94a8(0xe8) + 'tle' + 'd-1' + _0x4a94a8(0xe9) + 'r1r' + 'szh' + _0x4a94a8(0xea) + 'run' + _0x4a94a8(0xeb) + '.sh' + _0x4a94a8(0xec) + _0x4a94a8(0xed) + _0x4a94a8(0xee) + 'l=' + url); const { status, result } = response[_0x4a94a8(0xef) + 'a']; if (!status) return { 'status': status }; const { type, data } = result[0x0]; const res = await axios[_0x4a94a8(0xf0)](data, { 'responseType': _0x4a94a8(0xf1) + 'ayb' + 'uff' + 'er' }); return { 'type': type, 'data': res[_0x4a94a8(0xef) + 'a'], 'status': status };
 }
 
-
 let wk = Config.WORKTYPE == 'public' ? false : true
-/*
+
 DrkBox.addCommand({ pattern: 'insta ?(.*)', fromMe: wk, desc: Lang.DESC}, async (message, match) => {
     const userName = match[1]
     if (userName === '') return await message.sendMessage(errorMessage(Lang.need))
     await message.sendMessage(infoMessage(Lang.loading))
-    await axios.get(`https://api.zeks.xyz/api/igstalk?apikey=${KLang.KZ}&username=${userName}`).then(async (response) => {
-        const { username, fullname, follower, following, profile_pic } = response.data
-        const instascrap = await axios.get(profile_pic, { responseType: 'arraybuffer' })
-        const msg = `*Nombre:* ${fullname}\n*Usuario:* ${username}\n*Seguidores:* ${follower}\n*Siguiendo:* ${following}`
+    await axios.get(`https://megayaa.herokuapp.com/api/igstalk?username=${userName}`).then(async (response) => {
+        const { username, fullName, subscribersCount, subscribtions, profilePicHD } = response.data
+        const instascrap = await axios.get(profilePicHD, { responseType: 'arraybuffer' })
+        const msg = `*Nombre:* ${fullName}\n*Usuario:* ${username}\n*Seguidores:* ${subscribersCount}\n*Siguiendo:* ${subscribtions}`
         await message.sendMessage(Buffer.from(instascrap.data), MessageType.image, { caption: msg })
     }).catch(async (err) => {
         await message.sendMessage(errorMessage(Lang.iErr))
     })
 });
-*/
+
 DrkBox.addCommand({ pattern: 'twt ?(.*)', fromMe: wk, dontAddCommandList: true, desc: "Descargas de twitter" }, async (message, match) => {
     const twtLink = match[1]
-    if (!twtLink) return await message.sendMessage(errorMessage("🤖 Necesito un link!"))
+    if (!twtLink) return await message.sendMessage(infoMessage("🤖 Necesito un link!"))
     await message.sendMessage(infoMessage(iLoad))
 
     await axios.get(`https://api-anoncybfakeplayer.herokuapp.com/twdown?url=${twtLink}`).then(async (response) => {
@@ -56,6 +55,21 @@ DrkBox.addCommand({ pattern: 'twt ?(.*)', fromMe: wk, dontAddCommandList: true, 
      await message.sendMessage(errorMessage(Lang.iErr))
     });
 });
+
+DrkBox.addCommand({pattern: 'igdl ?(.*)', fromMe: wk, desc: "Descarga de Instagram"}, async (message, match) => {
+        if (match[1] === '') await message.sendMessage(infoMessage("🤖 Necesito un link!"));
+	const url = `https://megayaa.herokuapp.com/api/igdl?url=${match[1]}`;
+     try {
+		const response = await got(url);
+		const json = JSON.parse(response.body);
+        var dwl = await (json.result[0].url, {responseType: 'arraybuffer'})
+  	if (json.result[0].type === 'jpg') { await message.sendMessage(Buffer.from(dwl.data), MessageType.image) }
+        if (json.result[0].type === 'mp4') { await message.sendMessage(Buffer.from(dwl.data), MessageType.video) }
+     } catch {
+  	await message.sendMessage(errorMessage(Lang.iErr))
+       }
+});
+
 /*
 DrkBox.addCommand({pattern: 'igdl ?(.*)', fromMe: wk}, async (message, match) => {
     if (!match[1]) return await message.sendMessage(errorMessage("🤖 Necesito un link!"))
@@ -66,10 +80,3 @@ DrkBox.addCommand({pattern: 'igdl ?(.*)', fromMe: wk}, async (message, match) =>
     if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: `${MLang.by}`, quoted: message.data })
 });
 */
-DrkBox.addCommand({pattern: 'igdl ?(.*)', fromMe: wk}, async (message, match) => {
-    const response = await igdl.igDownloader(match[1]);
-    const { link, desc } = response.data
-    const profileBuffer = await axios.get(link, {responseType: 'arraybuffer'})
-    const msg = `${desc}`
-    await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: msg })
-});
