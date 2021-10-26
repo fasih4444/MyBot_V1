83afee8d605f7ce48cd658179b78ac8988665ec0
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-10-25 19:39:37 -0500

    Update others.js

--- plugins/others.js
+++ plugins/others.js
@@ -87,10 +87,10 @@ DrkBox.addCommand({pattern: 'mediafire ?(.*)', fromMe: wk}, async (message, matc
   if (!match[1]) return await message.sendMessage(errorMessage("🤖 Necesito un link!"))
 
   await axios.get(`https://drkbot-rest.herokuapp.com/api/dbot/down/mediafire?url=${match[1]}&apikey=${KLang.rest}`).then(async (response) => {
-        const { title, link } = response.data.result
+        const { link } = response.data.result
     	const profileBuffer = await axios.get(link, { responseType: 'arraybuffer' })
         await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.document)
   }).catch (async (err) => {
-     await message.sendMessage(iErr, `o el tamaño de descarga supera los limites de WhatsApp, descargalo de forma externa.\n\n*Nombre:* ${title}\n*Link:* ${link}`, MessageType.text)
+     await message.sendMessage(iErr, `o el tamaño de descarga supera los limites de WhatsApp, descargalo de forma externa.\n\nLink:* ${link}`, MessageType.text)
     });
 });
