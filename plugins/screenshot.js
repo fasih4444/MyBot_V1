c3748f802014a96bf57561981539088f74a7f136
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-11-12 13:11:25 -0500

    Update screenshot.js

--- plugins/screenshot.js
+++ plugins/screenshot.js
@@ -7,6 +7,7 @@ DrkBot - Ian VanH
 const DrkBox = require('../events');
 const { MessageType, Mimetype } = require('@adiwajshing/baileys');
 const HeartBot = require('drkbot-npm');
+const axios = require('axios');
 const Config = require('../config');
 
 const Language = require('../language');
