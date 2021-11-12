49471650966d6b727de267f344e19105356050e4
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-11-12 12:28:28 -0500

    Update screenshot.js

--- plugins/screenshot.js
+++ plugins/screenshot.js
@@ -12,19 +12,18 @@ const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('webss');
 const MLang = Language.getString('messages');
+const Klang = Language.getstring('keys');
 
 let wk = Config.WORKTYPE == 'public' ? false : true
 
-DrkBox.addCommand({pattern: 'ss', fromMe: wk, desc: Lang.desc_ss}, (async (message, match) => {
-  await message.sendMessage('🤖 *Comando en mantenimiento, disculpa las molestias.*');
-/*if (match[1] === '') return await message.sendMessage(Lang.need_ss);
-  var bufferdata = ''
-  try {
-    var enc = await HeartBot.shot(match[1])
-    bufferdata = enc
-  } catch {
-    return await message.client.sendMessage(message.jid,'🤖 Parece que hay un Error!', MessageType.text);
-  }
-  await message.sendMessage(Buffer.from(bufferdata), MessageType.image, {mimetype: Mimetype.png, caption: `${MLang.by}`})
-*/
-}));
+DrkBox.addCommand({pattern: 'ss ?(.*)', fromMe: wk}, async (message, match) => {
+     if (!match[1]) return await message.sendMessage("⚠️: 🤖 *Necesito un link!*")
+
+     await axios.get(`https://api.apiflash.com/v1/urltoimage?access_key=${KLang.SS}&full_page=true&format=png&response_type=json&url=${match[1]}`).then(async (response) => {
+        const { url } = response.data
+    	const profileBuffer = await axios.get(url, { responseType: 'arraybuffer' })
+        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { mimetype: Mimetype.png, caption: `${MLang.by}`, quoted: message.data })
+  }).catch (async (err) => {
+    await message.sendMessage('🛑: 🤖 *Parece que tenemos un error!*')
+    });
+});
