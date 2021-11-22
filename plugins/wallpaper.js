0ff05dd47ce91faaf134d2e88d1b1137c992e43a
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-11-22 06:27:03 -0500

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -21,7 +21,7 @@ let wk = Config.WORKTYPE == 'public' ? false : true
 DrkBox.addCommand({pattern: 'wallpaper ?(.*)', fromMe: wk, desc: Lang.WP}, (async (message, match) => {
         dbot.wallpaper(match[1]).then(async (result) => {
             var wall = Math.floor(result.length*Math.random());
-            var respoimage = await axios.get(`$(result[wall]`, { responseType: 'arraybuffer' })
+            var respoimage = await axios.get(`${result[wall]}`, { responseType: 'arraybuffer' })
             await message.client.sendMessage(message.jid, Buffer.from(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `${MLang.by}`})
         });
 }));
