0e63a3b75b832a915b176e43d50440ecc88de0be
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-11-25 22:52:13 -0500

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -23,23 +23,14 @@ DrkBox.addCommand({pattern: 'img ?(.*)', fromMe: wk, desc: iLang.IMG_DESC}, (asy
         if (!match[1]) return await message.sendMessage(infoMessage(iLang.NEED_WORDS));
         await message.sendMessage(Lang.NEW_IMG,match[1],MessageType.text);
         gis(match[1], async (error, result) => {
-            var wall_a = Math.floor(result.length*Math.random());
-    	    var wall_b = Math.floor(result.length*Math.random());
-    	    var wall_c = Math.floor(result.length*Math.random());
-    	    var wall_d = Math.floor(result.length*Math.random());
-    	    var wall_e = Math.floor(result.length*Math.random());
+            for (var i = Math.floor(result.length*Math.random()); i++) {
+                var get = got(result[i].url, {https: {rejectUnauthorized: false}});
+                var stream = get.buffer();
 
-            var image_a = await axios.get(`${result[wall_a].url}`, { responseType: 'arraybuffer' })
-            var image_b = await axios.get(`${result[wall_b].url}`, { responseType: 'arraybuffer' })
-            var image_c = await axios.get(`${result[wall_c].url}`, { responseType: 'arraybuffer' })
-            var image_d = await axios.get(`${result[wall_d].url}`, { responseType: 'arraybuffer' })
-            var image_e = await axios.get(`${result[wall_e].url}`, { responseType: 'arraybuffer' })
-
-            await message.client.sendMessage(message.jid, Buffer.from(image_a.data), MessageType.image, {mimetype: Mimetype.png})
-            await message.client.sendMessage(message.jid, Buffer.from(image_b.data), MessageType.image, {mimetype: Mimetype.png})
-            await message.client.sendMessage(message.jid, Buffer.from(image_c.data), MessageType.image, {mimetype: Mimetype.png})
-            await message.client.sendMessage(message.jid, Buffer.from(image_d.data), MessageType.image, {mimetype: Mimetype.png})
-            await message.client.sendMessage(message.jid, Buffer.from(image_e.data), MessageType.image, {mimetype: Mimetype.png})
+                stream.then(async (image) => {
+                    await message.client.sendMessage(message.jid,image, MessageType.image);
+                });
+            }
         });
 }));
 
