ecc50894e81ec65e108268a046a432a88c32bb77
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-11-25 19:57:07 -0500

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -50,7 +50,6 @@ const KLang = Language.getString('keys');
 const MLang = Language.getString('messages');
 
 const wiki = require('wikijs').default;
-var gis = require('g-i-s');
 
 var dlang_dsc = ''
 var closer_res = ''
@@ -58,47 +57,6 @@ var dlang_lang = ''
 var dlang_similarity = ''
 var dlang_other = ''
 var dlang_input = ''
-
-if (config.LANG == 'TR') {
-    dlang_dsc = 'Yanıtlanan mesajın dilini tahmin eder.'
-    closer_res = 'En Yakın Sonuç:'
-    dlang_lang = 'Dil:'
-    dlang_similarity = 'Benzerlik:'
-    dlang_other = 'Diğer Diller'
-    dlang_input = 'İşlenen Metin:'
-}
-if (config.LANG == 'EN') {
-    dlang_dsc = 'Guess the language of the replied message.'
-    closer_res = 'Closest Result:'
-    dlang_lang = 'Language:'
-    dlang_similarity = 'Similarity:'
-    dlang_other = 'Other Languages'
-    dlang_input = 'Processed Text:'
-}
-if (config.LANG == 'AZ') {
-    dlang_dsc = 'Cavablanan mesajın dilini təxmin edin.'
-    closer_res = 'Ən yaxın nəticə:'
-    dlang_lang = 'Dil:'
-    dlang_similarity = 'Bənzərlik:'
-    dlang_other = 'Başqa Dillər'
-    dlang_input = 'İşlənmiş Mətn:'
-}
-if (config.LANG == 'ML') {
-    dlang_dsc = 'മറുപടി നൽകിയ സന്ദേശത്തിന്റെ ഭാഷ ess ഹിക്കുക.'
-    closer_res = 'ഏറ്റവും അടുത്ത ഫലം:'
-    dlang_lang = 'നാവ്:'
-    dlang_similarity = 'സമാനത:'
-    dlang_other = 'മറ്റ് ഭാഷകൾ'
-    dlang_input = 'പ്രോസസ്സ് ചെയ്ത വാചകം:'
-}
-if (config.LANG == 'HI') {
-    dlang_dsc = 'उत्तर दिए गए संदेश की भाषा का अनुमान लगाएं'
-    closer_res = 'निकटतम परिणाम:'
-    dlang_lang = 'जुबान:'
-    dlang_similarity = 'समानता:'
-    dlang_other = 'अन्य भाषाएँ'
-    dlang_input = 'संसाधित पाठ:'
-}
 if (config.LANG == 'ES') {
     dlang_dsc = 'Adivina el idioma del mensaje respondido.'
     closer_res = 'Resultado más cercano:'
@@ -107,29 +65,13 @@ if (config.LANG == 'ES') {
     dlang_other = 'Otros idiomas:'
     dlang_input = 'Texto procesado:'
 }
-if (config.LANG == 'PT') {
-    dlang_dsc = 'Adivinhe o idioma da mensagem respondida.'
-    closer_res = 'Resultado mais próximo:'
-    dlang_lang = 'Língua:'
-    dlang_similarity = 'Similaridade:'
-    dlang_other = 'Outras línguas'
-    dlang_input = 'Texto Processado:'
-}
-if (config.LANG == 'ID') {
-    dlang_dsc = 'Tebak bahasa pesan yang dibalas.'
-    closer_res = 'Hasil Terdekat:'
-    dlang_lang = 'Lidah:'
-    dlang_similarity = 'Kesamaan:'
-    dlang_other = 'Bahasa Lainnya'
-    dlang_input = 'Teks yang Diproses:'
-}
-if (config.LANG == 'RU') {
-    dlang_dsc = 'Угадай язык ответного сообщения.'
-    closer_res = 'Ближайший результат:'
-    dlang_lang = 'Язык:'
-    dlang_similarity = 'Сходствo:'
-    dlang_other = 'Другие языки'
-    dlang_input = 'Обработанный текст:'
+if (config.LANG == 'EN') {
+    dlang_dsc = 'Guess the language of the replied message.'
+    closer_res = 'Closest Result:'
+    dlang_lang = 'Language:'
+    dlang_similarity = 'Similarity:'
+    dlang_other = 'Other Languages'
+    dlang_input = 'Processed Text:'
 }
 
 if (config.WORKTYPE == 'private') {
@@ -154,55 +96,6 @@ if (config.WORKTYPE == 'private') {
     var alr_off = ''
     var succ_on = ''
     var succ_off = ''
-    if (config.LANG == 'TR') {
-        l_dsc = 'Antilink aracını etkinleştirir.'
-        alr_on = 'Antilink halihazırda açık!'
-        alr_off = 'Antilink halihazırda kapalı!'
-        succ_on = 'Antilink Başarıyla Açıldı!'
-        succ_off = 'Antilink Başarıyla Kapatıldı!'
-    }
-    if (config.LANG == 'EN') {
-        l_dsc = 'Activates the Antilink tool.'
-        alr_on = 'Antilink is already open!'
-        alr_off = 'Antilink is currently closed!'
-        succ_on = 'Antilink Opened Successfully!'
-        succ_off = 'Antilink Closed Successfully!'
-    }
-    if (config.LANG == 'AZ') {
-        l_dsc = 'Antilink alətini aktivləşdirir.'
-        alr_on = 'Antilink hazırda açıqdır!'
-        alr_off = 'Antilink hazırda bağlıdır!'
-        succ_on = 'Antilink Uğurla Açıldı!'
-        succ_off = 'Antilink Uğurla Bağlandı!'
-    }
-    if (config.LANG == 'HI') {
-        l_dsc = 'एंटीलिंक टूल को सक्रिय करता है।'
-        alr_on = 'एंटीलिंक पहले से ही खुला है!'
-        alr_off = 'एंटीलिंक वर्तमान में बंद है!'
-        succ_on = 'एंटीलिंक सफलतापूर्वक खोला गया!'
-        succ_off = 'एंटीलिंक सफलतापूर्वक बंद!'
-    }
-    if (config.LANG == 'ML') {
-        l_dsc = 'ആന്റിലിങ്ക് ഉപകരണം സജീവമാക്കുന്നു.'
-        alr_on = 'ആന്റിലിങ്ക് ഇതിനകം തുറന്നു!'
-        alr_off = 'ആന്റിലിങ്ക് നിലവിൽ അടച്ചിരിക്കുന്നു!'
-        succ_on = 'ആന്റിലിങ്ക് വിജയകരമായി തുറന്നു!'
-        succ_off = 'ആന്റിലിങ്ക് വിജയകരമായി അടച്ചു!'
-    }
-    if (config.LANG == 'PT') {
-        l_dsc = 'Ativa a ferramenta Antilink.'
-        alr_on = 'O Antilink já está aberto!'
-        alr_off = 'Antilink está fechado no momento!'
-        succ_on = 'Antilink aberto com sucesso!'
-        succ_off = 'Antilink fechado com sucesso!'
-    }
-    if (config.LANG == 'RU') {
-        l_dsc = 'Активирует инструмент Antilink.'
-        alr_on = 'Антилинк уже открыт!'
-        alr_off = 'Антилинк сейчас закрыт!'
-        succ_on = 'Антилинк успешно открыт!'
-        succ_off = 'Антилинк успешно закрыт!'
-    }
     if (config.LANG == 'ES') {
         l_dsc = 'Activa la herramienta Antilink.'
         alr_on = '¡Antilink ya está abierto!'
@@ -210,12 +103,12 @@ if (config.WORKTYPE == 'private') {
         succ_on = '¡Antilink se abrió con éxito!'
         succ_off = 'Antilink cerrado correctamente!'
     }
-    if (config.LANG == 'ID') {
-        l_dsc = 'Mengaktifkan alat Antilink.'
-        alr_on = 'Antilink sudah terbuka!'
-        alr_off = 'Antilink saat ini ditutup!'
-        succ_on = 'Antilink Berhasil Dibuka!'
-        succ_off = 'Antilink Berhasil Ditutup!'
+    if (config.LANG == 'EN') {
+        l_dsc = 'Activates the Antilink tool.'
+        alr_on = 'Antilink is already open!'
+        alr_off = 'Antilink is currently closed!'
+        succ_on = 'Antilink Opened Successfully!'
+        succ_off = 'Antilink Closed Successfully!'
     }
     DrkBot.addCommand({pattern: 'antilink ?(.*)', fromMe: true, desc: l_dsc, usage: '.antilink on / off' }, (async (message, match) => {
         const anti_status = `${config.ANTİLİNK}`
@@ -251,55 +144,6 @@ if (config.WORKTYPE == 'private') {
     var alr_off_bio = ''
     var succ_on_bio = ''
     var succ_off_bio = ''
-    if (config.LANG == 'TR') {
-        auto_dsc = 'Biyografinize canlı saat ekleyin!'
-        alr_on_bio = 'Autobio halihazırda açık!'
-        alr_off_bio = 'Autobio halihazırda kapalı!'
-        succ_on_bio = 'Autobio Başarıyla Açıldı!'
-        succ_off_bio = 'Autobio Başarıyla Kapatıldı!'
-    }
-    if (config.LANG == 'EN') {
-        auto_dsc = 'Add live clock to your bio!'
-        alr_on_bio = 'Autobio is already open!'
-        alr_off_bio = 'Autobio is currently closed!'
-        succ_on_bio = 'Autobio Opened Successfully!'
-        succ_off_bio = 'Autobio Closed Successfully!'
-    }
-    if (config.LANG == 'AZ') {
-        auto_dsc = 'Bio-ya canlı saat əlavə et!'
-        alr_on_bio = 'Autobio hazırda açıqdır!'
-        alr_off_bio = 'Autobio hazırda bağlıdır!'
-        succ_on_bio = 'Autobio Uğurla Açıldı!'
-        succ_off_bio = 'Autobio Uğurla Bağlandı!'
-    }
-    if (config.LANG == 'HI') {
-        auto_dsc = 'अपने बायो में लाइव घड़ी जोड़ें!'
-        alr_on_bio = 'Autobio पहले से ही खुला है!'
-        alr_off_bio = 'Autobio वर्तमान में बंद है!'
-        succ_on_bio = 'Autobio सफलतापूर्वक खोला गया!'
-        succ_off_bio = 'Autobio सफलतापूर्वक बंद!'
-    }
-    if (config.LANG == 'ML') {
-        auto_dsc = 'നിങ്ങളുടെ ബയോയിലേക്ക് തത്സമയ ക്ലോക്ക് ചേർക്കുക!'
-        alr_on_bio = 'Autobio ഇതിനകം തുറന്നു!'
-        alr_off_bio = 'Autobio നിലവിൽ അടച്ചിരിക്കുന്നു!'
-        succ_on_bio = 'Autobio വിജയകരമായി തുറന്നു!'
-        succ_off_bio = 'Autobio വിജയകരമായി അടച്ചു!'
-    }
-    if (config.LANG == 'PT') {
-        auto_dsc = 'Adicione um relógio ao vivo à sua biografia!'
-        alr_on_bio = 'O Autobio já está aberto!'
-        alr_off_bio = 'Autobio está fechado no momento!'
-        succ_on_bio = 'Autobio aberto com sucesso!'
-        succ_off_bio = 'Autobio fechado com sucesso!'
-    }
-    if (config.LANG == 'RU') {
-        auto_dsc = 'Добавьте живые часы в свою биографию!'
-        alr_on_bio = 'Autobio уже открыт!'
-        alr_off_bio = 'Autobio сейчас закрыт!'
-        succ_on_bio = 'Autobio успешно открыт!'
-        succ_off_bio = 'Autobio успешно закрыт!'
-    }
     if (config.LANG == 'ES') {
         auto_dsc = '¡Agrega un reloj en vivo a tu biografía!'
         alr_on_bio = '¡Autobio ya está abierto!'
@@ -307,12 +151,12 @@ if (config.WORKTYPE == 'private') {
         succ_on_bio = '¡Autobio se abrió con éxito!'
         succ_off_bio = 'Autobio cerrado correctamente!'
     }
-    if (config.LANG == 'ID') {
-        auto_dsc = 'Tambahkan jam langsung ke bio Anda!'
-        alr_on_bio = 'Autobio sudah terbuka!'
-        alr_off_bio = 'Autobio saat ini ditutup!'
-        succ_on_bio = 'Autobio Berhasil Dibuka!'
-        succ_off_bio = 'Autobio Berhasil Ditutup!'
+    if (config.LANG == 'EN') {
+        auto_dsc = 'Add live clock to your bio!'
+        alr_on_bio = 'Autobio is already open!'
+        alr_off_bio = 'Autobio is currently closed!'
+        succ_on_bio = 'Autobio Opened Successfully!'
+        succ_off_bio = 'Autobio Closed Successfully!'
     }
     DrkBot.addCommand({pattern: 'autobio ?(.*)', fromMe: true, desc: auto_dsc, usage: '.autobio on / off' }, (async (message, match) => {
         const bio_status = `${config.AUTOBİO}`
@@ -485,23 +329,6 @@ if (config.WORKTYPE == 'private') {
         var info = await arama.rawContent();
         await message.client.sendMessage(message.jid, info, MessageType.text);
     }));
-
-    DrkBot.addCommand({pattern: 'img ?(.*)', fromMe: true, desc: Lang.IMG_DESC}, (async (message, match) => { 
-
-        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);
-        gis(match[1], async (error, result) => {
-            for (var i = 0; i < (result.length < 5 ? result.length : 5); i++) {
-                var get = got(result[i].url, {https: {rejectUnauthorized: false}});
-                var stream = get.buffer();
-                
-                stream.then(async (image) => {
-                    await message.client.sendMessage(message.jid,image, MessageType.image);
-                });
-            }
-
-            message.reply(Lang.IMG.format((result.length < 5 ? result.length : 5), match[1]));
-        });
-    }));
     /*
     DrkBot.addCommand({ pattern: 'github ?(.*)', fromMe: true, desc: Glang.GİTHUB_DESC }, async (message, match) => {
 
@@ -585,55 +412,6 @@ else if (config.WORKTYPE == 'public') {
     var alr_off = ''
     var succ_on = ''
     var succ_off = ''
-    if (config.LANG == 'TR') {
-        l_dsc = 'Antilink aracını etkinleştirir.'
-        alr_on = 'Antilink halihazırda açık!'
-        alr_off = 'Antilink halihazırda kapalı!'
-        succ_on = 'Antilink Başarıyla Açıldı!'
-        succ_off = 'Antilink Başarıyla Kapatıldı!'
-    }
-    if (config.LANG == 'EN') {
-        l_dsc = 'Activates the Antilink tool.'
-        alr_on = 'Antilink is already open!'
-        alr_off = 'Antilink is currently closed!'
-        succ_on = 'Antilink Opened Successfully!'
-        succ_off = 'Antilink Closed Successfully!'
-    }
-    if (config.LANG == 'AZ') {
-        l_dsc = 'Antilink alətini aktivləşdirir.'
-        alr_on = 'Antilink hazırda açıqdır!'
-        alr_off = 'Antilink hazırda bağlıdır!'
-        succ_on = 'Antilink Uğurla Açıldı!'
-        succ_off = 'Antilink Uğurla Bağlandı!'
-    }
-    if (config.LANG == 'HI') {
-        l_dsc = 'एंटीलिंक टूल को सक्रिय करता है।'
-        alr_on = 'एंटीलिंक पहले से ही खुला है!'
-        alr_off = 'एंटीलिंक वर्तमान में बंद है!'
-        succ_on = 'एंटीलिंक सफलतापूर्वक खोला गया!'
-        succ_off = 'एंटीलिंक सफलतापूर्वक बंद!'
-    }
-    if (config.LANG == 'ML') {
-        l_dsc = 'ആന്റിലിങ്ക് ഉപകരണം സജീവമാക്കുന്നു.'
-        alr_on = 'ആന്റിലിങ്ക് ഇതിനകം തുറന്നു!'
-        alr_off = 'ആന്റിലിങ്ക് നിലവിൽ അടച്ചിരിക്കുന്നു!'
-        succ_on = 'ആന്റിലിങ്ക് വിജയകരമായി തുറന്നു!'
-        succ_off = 'ആന്റിലിങ്ക് വിജയകരമായി അടച്ചു!'
-    }
-    if (config.LANG == 'PT') {
-        l_dsc = 'Ativa a ferramenta Antilink.'
-        alr_on = 'O Antilink já está aberto!'
-        alr_off = 'Antilink está fechado no momento!'
-        succ_on = 'Antilink aberto com sucesso!'
-        succ_off = 'Antilink fechado com sucesso!'
-    }
-    if (config.LANG == 'RU') {
-        l_dsc = 'Активирует инструмент Antilink.'
-        alr_on = 'Антилинк уже открыт!'
-        alr_off = 'Антилинк сейчас закрыт!'
-        succ_on = 'Антилинк успешно открыт!'
-        succ_off = 'Антилинк успешно закрыт!'
-    }
     if (config.LANG == 'ES') {
         l_dsc = 'Activa la herramienta Antilink.'
         alr_on = '¡Antilink ya está abierto!'
@@ -641,12 +419,12 @@ else if (config.WORKTYPE == 'public') {
         succ_on = '¡Antilink se abrió con éxito!'
         succ_off = 'Antilink cerrado correctamente!'
     }
-    if (config.LANG == 'ID') {
-        l_dsc = 'Mengaktifkan alat Antilink.'
-        alr_on = 'Antilink sudah terbuka!'
-        alr_off = 'Antilink saat ini ditutup!'
-        succ_on = 'Antilink Berhasil Dibuka!'
-        succ_off = 'Antilink Berhasil Ditutup!'
+    if (config.LANG == 'EN') {
+        l_dsc = 'Activates the Antilink tool.'
+        alr_on = 'Antilink is already open!'
+        alr_off = 'Antilink is currently closed!'
+        succ_on = 'Antilink Opened Successfully!'
+        succ_off = 'Antilink Closed Successfully!'
     }
     DrkBot.addCommand({pattern: 'antilink ?(.*)', fromMe: true, desc: l_dsc, usage: '.antilink on / off' }, (async (message, match) => {
         const anti_status = `${config.ANTİLİNK}`
@@ -682,55 +460,6 @@ else if (config.WORKTYPE == 'public') {
     var alr_off_bio = ''
     var succ_on_bio = ''
     var succ_off_bio = ''
-    if (config.LANG == 'TR') {
-        auto_dsc = 'Biyografinize canlı saat ekleyin!'
-        alr_on_bio = 'Autobio halihazırda açık!'
-        alr_off_bio = 'Autobio halihazırda kapalı!'
-        succ_on_bio = 'Autobio Başarıyla Açıldı!'
-        succ_off_bio = 'Autobio Başarıyla Kapatıldı!'
-    }
-    if (config.LANG == 'EN') {
-        auto_dsc = 'Add live clock to your bio!'
-        alr_on_bio = 'Autobio is already open!'
-        alr_off_bio = 'Autobio is currently closed!'
-        succ_on_bio = 'Autobio Opened Successfully!'
-        succ_off_bio = 'Autobio Closed Successfully!'
-    }
-    if (config.LANG == 'AZ') {
-        auto_dsc = 'Bio-ya canlı saat əlavə et!'
-        alr_on_bio = 'Autobio hazırda açıqdır!'
-        alr_off_bio = 'Autobio hazırda bağlıdır!'
-        succ_on_bio = 'Autobio Uğurla Açıldı!'
-        succ_off_bio = 'Autobio Uğurla Bağlandı!'
-    }
-    if (config.LANG == 'HI') {
-        auto_dsc = 'अपने बायो में लाइव घड़ी जोड़ें!'
-        alr_on_bio = 'Autobio पहले से ही खुला है!'
-        alr_off_bio = 'Autobio वर्तमान में बंद है!'
-        succ_on_bio = 'Autobio सफलतापूर्वक खोला गया!'
-        succ_off_bio = 'Autobio सफलतापूर्वक बंद!'
-    }
-    if (config.LANG == 'ML') {
-        auto_dsc = 'നിങ്ങളുടെ ബയോയിലേക്ക് തത്സമയ ക്ലോക്ക് ചേർക്കുക!'
-        alr_on_bio = 'Autobio ഇതിനകം തുറന്നു!'
-        alr_off_bio = 'Autobio നിലവിൽ അടച്ചിരിക്കുന്നു!'
-        succ_on_bio = 'Autobio വിജയകരമായി തുറന്നു!'
-        succ_off_bio = 'Autobio വിജയകരമായി അടച്ചു!'
-    }
-    if (config.LANG == 'PT') {
-        auto_dsc = 'Adicione um relógio ao vivo à sua biografia!'
-        alr_on_bio = 'O Autobio já está aberto!'
-        alr_off_bio = 'Autobio está fechado no momento!'
-        succ_on_bio = 'Autobio aberto com sucesso!'
-        succ_off_bio = 'Autobio fechado com sucesso!'
-    }
-    if (config.LANG == 'RU') {
-        auto_dsc = 'Добавьте живые часы в свою биографию!'
-        alr_on_bio = 'Autobio уже открыт!'
-        alr_off_bio = 'Autobio сейчас закрыт!'
-        succ_on_bio = 'Autobio успешно открыт!'
-        succ_off_bio = 'Autobio успешно закрыт!'
-    }
     if (config.LANG == 'ES') {
         auto_dsc = '¡Agrega un reloj en vivo a tu biografía!'
         alr_on_bio = '¡Autobio ya está abierto!'
@@ -738,12 +467,12 @@ else if (config.WORKTYPE == 'public') {
         succ_on_bio = '¡Autobio se abrió con éxito!'
         succ_off_bio = 'Autobio cerrado correctamente!'
     }
-    if (config.LANG == 'ID') {
-        auto_dsc = 'Tambahkan jam langsung ke bio Anda!'
-        alr_on_bio = 'Autobio sudah terbuka!'
-        alr_off_bio = 'Autobio saat ini ditutup!'
-        succ_on_bio = 'Autobio Berhasil Dibuka!'
-        succ_off_bio = 'Autobio Berhasil Ditutup!'
+    if (config.LANG == 'EN') {
+        auto_dsc = 'Add live clock to your bio!'
+        alr_on_bio = 'Autobio is already open!'
+        alr_off_bio = 'Autobio is currently closed!'
+        succ_on_bio = 'Autobio Opened Successfully!'
+        succ_off_bio = 'Autobio Closed Successfully!'
     }
     DrkBot.addCommand({pattern: 'autobio ?(.*)', fromMe: false, desc: auto_dsc, usage: '.autobio on / off' }, (async (message, match) => {
         const bio_status = `${config.AUTOBİO}`
