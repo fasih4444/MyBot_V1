adc7e8895d5e7c17ea82d2d6c90a55e9c71dc02d
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-11-25 19:34:33 -0500

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -21,9 +21,8 @@ let wk = Config.WORKTYPE == 'public' ? false : true
 
 DrkBox.addCommand({pattern: 'img ?(.*)', fromMe: wk, desc: iLang.IMG_DESC}, (async (message, match) => { 
         if (!match[1]) return await message.sendMessage(infoMessage(iLang.NEED_WORDS));
+        await message.sendMessage(Lang.NEW_IMG,match[1],MessageType.text);
         gis(match[1], async (error, result) => {
-      //    await message.client.sendMessage(message.jid,Lang.NEW_IMG,match[1],MessageType.text);
-
             var wall_a = Math.floor(result.length*Math.random());
     	    var wall_b = Math.floor(result.length*Math.random());
     	    var wall_c = Math.floor(result.length*Math.random());
@@ -47,24 +46,8 @@ DrkBox.addCommand({pattern: 'img ?(.*)', fromMe: wk, desc: iLang.IMG_DESC}, (asy
 DrkBox.addCommand({pattern: 'wallpaper ?(.*)', fromMe: wk, desc: iLang.IMG_DESC}, (async (message, match) => { 
         if (!match[1]) return await message.sendMessage(infoMessage(iLang.NEED_WORDS));
         dbot.pinterest(match[1]).then(async (result) => {
-      //    await message.client.sendMessage(message.jid,Lang.NEW_IMG,match[1],MessageType.text);
-
             var wall_a = Math.floor(result.length*Math.random());
-    	    var wall_b = Math.floor(result.length*Math.random());
-    	    var wall_c = Math.floor(result.length*Math.random());
-    	    var wall_d = Math.floor(result.length*Math.random());
-    	    var wall_e = Math.floor(result.length*Math.random());
-
             var image_a = await axios.get(`${result[wall_a]}`, { responseType: 'arraybuffer' })
-            var image_b = await axios.get(`${result[wall_b]}`, { responseType: 'arraybuffer' })
-            var image_c = await axios.get(`${result[wall_c]}`, { responseType: 'arraybuffer' })
-            var image_d = await axios.get(`${result[wall_d]}`, { responseType: 'arraybuffer' })
-            var image_e = await axios.get(`${result[wall_e]}`, { responseType: 'arraybuffer' })
-
             await message.client.sendMessage(message.jid, Buffer.from(image_a.data), MessageType.image, {mimetype: Mimetype.png})
-            await message.client.sendMessage(message.jid, Buffer.from(image_b.data), MessageType.image, {mimetype: Mimetype.png})
-            await message.client.sendMessage(message.jid, Buffer.from(image_c.data), MessageType.image, {mimetype: Mimetype.png})
-            await message.client.sendMessage(message.jid, Buffer.from(image_d.data), MessageType.image, {mimetype: Mimetype.png})
-            await message.client.sendMessage(message.jid, Buffer.from(image_e.data), MessageType.image, {mimetype: Mimetype.png})
         });
 }));
