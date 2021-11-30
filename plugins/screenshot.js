b7784de8caadd9f89b71a09cac64c5c73a15223a
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-11-30 00:43:25 -0500

    Update screenshot.js

--- plugins/screenshot.js
+++ plugins/screenshot.js
@@ -20,10 +20,10 @@ let wk = Config.WORKTYPE == 'public' ? false : true
 DrkBox.addCommand({pattern: 'ss ?(.*)', fromMe: wk}, async (message, match) => {
      if (!match[1]) return await message.sendMessage("⚠️: 🤖 *Necesito un link!*")
 
-     await axios.get(`https://api.apiflash.com/v1/urltoimage?access_key=${KLang.SS}&full_page=true&format=png&response_type=json&url=${match[1]}`).then(async (response) => {
-        const { url } = response.data
-    	const profileBuffer = await axios.get(url, { responseType: 'arraybuffer' })
-        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { mimetype: Mimetype.png, caption: `${MLang.by}`, quoted: message.data })
+     await axios.get(`https://drkbot.vercel.app/api/new/ss?&url=${match[1]}&apikey=${KLang.rest}`).then(async (response) => {
+        const { url } = response.data.result
+    	const rest_ss = await axios.get(url, { responseType: 'arraybuffer' })
+        await message.sendMessage(Buffer.from(rest_ss.data), MessageType.image, { mimetype: Mimetype.png, caption: `${MLang.by}`, quoted: message.data })
   }).catch (async (err) => {
     await message.sendMessage('🛑: 🤖 *Parece que tenemos un error!*')
     });
