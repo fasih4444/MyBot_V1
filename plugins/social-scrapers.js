fc84e7899520c91ffaa9b38f4d699b28b5155c92
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-12-19 00:36:26 -0500

    Update social-scrapers.js

--- plugins/social-scrapers.js
+++ plugins/social-scrapers.js
@@ -8,6 +8,7 @@ const DrkBox = require('../events');
 const { MessageType } = require('@adiwajshing/baileys');
 const { errorMessage, infoMessage } = require('../helpers');
 const Config = require('../config');
+const ( igDownloader } = require('./datos.js')
 const dbot = require('dbot-api');
 const axios = require('axios');
 const fs = require('fs');
@@ -25,44 +26,28 @@ let wk = Config.WORKTYPE == 'public' ? false : true
 DrkBox.addCommand({ pattern: 'insta ?(.*)', fromMe: wk, desc: Lang.DESC}, async (message, match) => {
     const userName = match[1]
     if (!userName) return await message.sendMessage(errorMessage(Lang.need))
-    await message.sendMessage(infoMessage(Lang.loading))
-    await axios.get(`https://megayaa.herokuapp.com/api/igstalk?username=${userName}`).then(async (response) => {
-        const { username, fullName, subscribersCount, subscribtions, profilePicHD } = response.data
-        const instascrap = await axios.get(profilePicHD, { responseType: 'arraybuffer' })
-        const msg = `*Nombre:* ${fullName}\n*Usuario:* ${username}\n*Seguidores:* ${subscribersCount}\n*Siguiendo:* ${subscribtions}`
-        await message.sendMessage(Buffer.from(instascrap.data), MessageType.image, { caption: msg })
-    }).catch(async (err) => {
-        await message.sendMessage(errorMessage(Lang.iErr))
-    })
-});
+    await message.sendMessage(infoMessage(Lang.load))
 
-DrkBox.addCommand({ pattern: 'twt ?(.*)', fromMe: wk, dontAddCommandList: true, desc: "Descargas de twitter" }, async (message, match) => {
-    const twtLink = match[1]
-    if (!twtLink) return await message.sendMessage(infoMessage("🤖 Necesito un link!"))
-    await message.sendMessage(infoMessage(iLoad))
-
-    await axios.get(`https://api-anoncybfakeplayer.herokuapp.com/twdown?url=${twtLink}`).then(async (response) => {
-        const { format, result } = response.data
-        const profileBuffer = await axios.get(result, {responseType: 'arraybuffer'})
-        const msg = `${format}`
-    	if (msg === 'Image/jpg or png') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: `${MLang.by}`, quoted: message.data })}
-    	if (msg === 'video/mp4') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: `${MLang.by}`, quoted: message.data })}
-    }).catch (async (err) => {
-     await message.sendMessage(errorMessage(Lang.iErr))
-    });
+    res = await dbot.igstalk(userName)
+    ytm = res
+    const profileBuffer = await axios.get(`${ytm.profilePicHD}`, {responseType: 'arraybuffer'})
+    msg = `*Nombre:* ${ytm.fullName}\n*Usuario:* ${ytm.username}\n*Seguidores:* ${ytm.followers}\n*Siguiendo:* ${ytm.following}`
+    await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: msg, quoted: message.data })
+                
 });
 
 DrkBox.addCommand({pattern: 'igdl ?(.*)', fromMe: wk, desc: "Descarga de Instagram"}, async (message, match) => {
+  try{
     if (!match[1]) return await message.sendMessage(infoMessage("🤖 Necesito un link!"));
     await message.sendMessage(infoMessage(iLoad))
 
-    await axios.get(`https://drkbot.vercel.app/api/dbot/down/igdl?url=${match[1]}&apikey=${KLang.rest}`).then(async (response) => {
-        const { downloadUrl, type } = response.data.result
-        const profileBuffer = await axios.get(downloadUrl, {responseType: 'arraybuffer'})
-        const msg = `${type}`
-        if (msg === 'video') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: `${MLang.by}`, quoted: message.data })}
-        if (msg === 'image') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: `${MLang.by}`, quoted: message.data })}
-    }).catch (async (err) => {
+    res = await igDownloader(match[1])
+    ytm = res.result
+    const profileBuffer = await axios.get(`${ytm.link}`, {responseType: 'arraybuffer'})
+    const msg = `${ytm.link}`
+     if (msg.includes('.mp4')) { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: `${MLang.by}`, quoted: message.data })}
+     if (msg.includes('.jpg')) { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: `${MLang.by}`, quoted: message.data })}
+  } catch {
      await message.sendMessage(errorMessage(Lang.iErr))
-    });
+  }
 });
