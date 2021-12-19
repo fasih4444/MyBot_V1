605433df0195781d206543f9c411501fe0bf6bf9
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-12-19 13:29:11 -0500

    Create buttons.js

---
+++ plugins/buttons.js
@@ -0,0 +1,10 @@
+const DrkBox = require('../events');
+const Config = require('../config');
+const { MessageType } = require('@adiwajshing/baileys')
+
+let wk = Config.WORKTYPE == 'public' ? false : true
+
+
+DrkBox.addCommand({ pattern: 'test', fromMe: wk}, (async (message, match) => {
+
+}));
