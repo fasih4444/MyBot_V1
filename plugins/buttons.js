bf9c00059e27d45ff200f103df7d12d5a110b4bf
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-12-19 14:59:53 -0500

    Update buttons.js

--- plugins/buttons.js
+++ plugins/buttons.js
@@ -7,4 +7,26 @@ let wk = Config.WORKTYPE == 'public' ? false : true
 
 DrkBox.addCommand({ pattern: 'test', fromMe: wk}, (async (message, match) => {
 
+   const buttonMessage = {
+            contentText: '✪〘 *FUNCIONANDO* 〙✪',
+            footerText: 'Prueba',
+            buttons: [
+                {
+                    buttonId: `/menu`,
+                    buttonText: {
+                        displayText: "📒 MENU"
+                    },
+                    "type": "RESPONSE"
+                },
+                {
+                     buttonId: `/ping`,
+                     buttonText: {
+                         displayText: "🪀 PING"
+                    },
+                    "type": "RESPONSE"
+                },
+            ],
+            headerType: 1
+        }
+    await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage)
 }));
