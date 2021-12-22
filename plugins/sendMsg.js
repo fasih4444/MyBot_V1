c60e2c61a6e07118f39d3da99bd83fabf798f36a
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2021-12-22 09:50:50 -0500

    Update sendMsg.js

--- plugins/sendMsg.js
+++ plugins/sendMsg.js
@@ -12,6 +12,22 @@ const base64 = require('base-64');
 
 let wk = Config.WORKTYPE == 'public' ? false : true
 
+DrkBox.addCommand({pattern: 'reglas ?(.*)', fromMe: wk}, async (message, match) => {
+    const num = match[1]
+    const id = `${num}@s.whatsapp.net`
+    const idCod = base64.encode(`${message.jid}`)
+
+    await message.client.sendMessage(id,
+        'Hola soy 🤖 *DrkBot*\n' +
+        'Te están invitando a un chat anonimo, para poder responderle a esa persona vas a necesitar el codigo de respuesta de ese chat.\n' +
+        '*Las reglas de este chat son las siguientes:*\n' +
+        '1. Al final de este chat te va a llegar un mensaje con un codigo el cual podras usar para responder.\n' +
+        '2. Para responder es de la siguiente forma.\n' +
+        '*/send codigo respuesta + mensaje a responder*\n' +
+        '⚠️ El signo de mas(+) es el que separa el codigo del mensaje a responder, que no se te olvide colocarlo.', MessageType.text)
+    await message.client.sendMessage(id, idcod, MessageType.text)
+})
+
 DrkBox.addCommand({pattern: 'send ?(.*)', fromMe: wk}, async (message, match) => {
    if (!match[1]) return await message.sendMessage(infoMessage('🤖 *Envíale un mensaje a alguien de forma anonima por medio del BOT*\nForma de usar el comando.\n\n/send número al que le vas a enviar el msj, signo de + , mensaje a enviar\n\n/send 57xxxxxxx+Hola como estas.'))
 
@@ -20,24 +36,15 @@ DrkBox.addCommand({pattern: 'send ?(.*)', fromMe: wk}, async (message, match) =>
     const id = `${text[0]}@s.whatsapp.net`
     const msg = `${text[1]}`
 
-    const idCod = base64.encode(`${message.jid}`)
-
     var exists = await message.client.isOnWhatsApp(`${text[0]}`)
     if (exists) {
-       await message.client.sendMessage(id,
-         'Hola soy 🤖 *DrkBot*\n' +
-         'Te han enviado esté mensaje por medio de chat anonimo.\n\n' +
-         `*Mensaje:* ${msg}\n\n` +
-         'Para responder a esté mensaje escribe\n/rsend codigoRespuesta+mensaje de respuesta', MessageType.text)
-       await message.client.sendMessage(id, 'Tu codigo para responder es:', MessageType.text)
-       await message.client.sendMessage(id, `${idCod}`, MessageType.text)
-       await message.client.sendMessage(message.jid, '🤖 Su mensaje fue enviado con éxito.', MessageType.text)
-    } else {
+       await message.client.sendMessage(id, `🤖 Hola\n*Mensaje:* ${msg}`, MessageType.text)
+    } else if {
        await message.client.sendMessage(message.jid, '🤖 Su mensaje no pudo ser enviado.\nEl número ingresado no está registrado en whatsapp o está mal escrito.\n\nRecuerda que el número debe ser escrito en formato internacional, codigo del pais más número telefonico.', MessageType.text)
     }
 })
 
-DrkBox.addCommand({pattern: 'rsend ?(.*)', fromMe: wk}, async (message, match) => {
+DrkBox.addCommand({pattern: 'resp ?(.*)', fromMe: wk}, async (message, match) => {
    if (!match[1]) return await message.sendMessage(infoMessage('🤖 Forma de responder.\n\n/rsend codigo de respuesta, signo de + , mensaje de respuesta\n\n/rsend xxxxxxxx+mensaje a responder'))
 
     const codNum = match[1]
@@ -46,8 +53,13 @@ DrkBox.addCommand({pattern: 'rsend ?(.*)', fromMe: wk}, async (message, match) =
     const id = idDecod
     const msg = `${text[1]}`
 
-    await message.client.sendMessage(id,
-         'Hola soy 🤖 *DrkBot*\n' +
+    var exists = await message.client.isOnWhatsApp(id)
+    if (exists) {
+        await message.client.sendMessage(id,
+         '🤖 Hola\n' +
          `Tu mensaje de respuesta de ${message.jid} es:\n\n` +
          `*Mensaje:* ${msg}`, MessageType.text)
+    } else if {
+        await message.client.sendMessage(message.jid, '🤖 Su respuesta no pudo ser enviada.\nEl formato de respuesta no es correcto.', MessageType.text)
+    }
 })
