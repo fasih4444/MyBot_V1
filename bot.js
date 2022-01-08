6c29736896d1fb3b556cd7a0d7f09c09036af440
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2022-01-08 00:43:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -315,22 +315,33 @@ async function myBot () {
     })
     DrkBotCN.on('chat-update', async m => {
         if (!m.hasNewMessage) return;
-        if (!m.messages) return;
+        if (!m.messages && !m.count) return;
         let msg = m.messages.all()[0];
-        
-        if (msg.MessageType !== 'buttonsResponseMessage' && msg.type !== 1) return
-        let idMessage = msg.message.selectedButtonId
-        let isIdMessage = false, prefix
-
-        const type = Object.keys(msg.message)[0]
-    selectedButton = (msg.MessageType == 'buttonsResponseMessage') ? msg.message.buttonsResponseMessage.selectedButtonId : ''
-    responseButton = (msg.MessageType == 'listResponseMessage') ? msg.message.listResponseMessage.title : ''
-
 
         if (msg.key && msg.key.remoteJid == 'status@broadcast') return;
         if (config.NO_ONLINE) {
             await DrkBotCN.updatePresence(msg.key.remoteJid, Presence.unavailable);
         }
+
+// ######## PRUEBA ########
+const from = msg.key.remoteJid
+msg.message = (Object.keys(msg.message)[0] === 'ephemeralMessage') ? msg.message.ephemeralMessage.message : msg.message
+const type = Object.keys(msg.message)[0]
+const isGroup = from.endsWith('@g.us')
+const sender = msg.key.fromMe ? DrkBotCN.user.jid : isGroup ? msg.participant : msg.key.remoteJid
+const conts = msg.key.fromMe ? DrkBotCN.user.jid : DrkBotCN.contacts[sender] || {
+   notify: jid.replace(/@.+/, '')
+}
+let pushname = msg.key.fromMe ? DrkBotCN.user.name : conts.notify || conts.vname || conts.name || '*Amigo*'
+
+        
+        if (msg.MessageType !== 'buttonsResponseMessage' && msg.type !== 1) return
+        let idMessage = msg.message.selectedButtonId
+        let isIdMessage = false, prefix
+
+    selectedButton = (msg.MessageType == 'buttonsResponseMessage') ? msg.message.buttonsResponseMessage.selectedButtonId : ''
+    responseButton = (msg.MessageType == 'listResponseMessage') ? msg.message.listResponseMessage.title : ''
+// ######## END PRUEBA ########
 
         // ==================== Greetings ====================
         if (msg.messageStubType === 32 || msg.messageStubType === 28) {
