0419f89650681b58b8727f8da00628395cfc5884
DrkBotBase <70702685+DrkBotBase@users.noreply.github.com>
2022-01-15 20:46:07 -0500

    prueba 1

--- plugins/anuncios.js
+++ plugins/anuncios.js
@@ -3,3 +3,22 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 DrkBot - Ian VanH
 */
+
+const DrkBox = require('../events');
+const { MessageType, Mimetype } = require('@adiwajshing/baileys');
+const { getBuffer } = require('../whatsasena/');
+const Config = require('../config');
+
+let wk = Config.WORKTYPE == 'public' ? false : true
+
+DrkBox.addCommand({pattern: 'dif ?(.*)', fromMe: wk}, (async (message, match) => {
+	const msg = match[1]
+	const getAll = await message.client.chats.all()
+	getAll.forEach(async chats => {
+    	if(chats.jid.endsWith('@s.whatsapp.net')) {
+    		await message.client.sendMessage(message.jid, msg, MessageType.text)
+    	} else {
+    		await message.client.sendMessage(message.jid, msg, MessageType.text)
+    	}
+	})
+}));
