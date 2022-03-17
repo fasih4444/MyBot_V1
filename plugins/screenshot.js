5ae856bc5631f34c93b91f1e8dcdad4e7706dd76
Ian VanH <70702685+DrkBotBase@users.noreply.github.com>
2022-03-17 09:50:57 -0500

    Update screenshot.js

--- plugins/screenshot.js
+++ plugins/screenshot.js
@@ -19,8 +19,8 @@ let wk = Config.WORKTYPE == 'public' ? false : true
 DrkBox.addCommand({pattern: 'ss ?(.*)', fromMe: wk}, async (message, match) => {
      if (!match[1]) return await message.sendMessage("⚠️: 🤖 *Necesito un link!*")
 
-     await axios.get(`https://drkbot.vercel.app/api/new/ss?&url=${match[1]}&apikey=${KLang.rest}`).then(async (response) => {
-        const { url } = response.data.result
+     await axios.get(`https://api.apiflash.com/v1/urltoimage?access_key=${KLang.ss}&full_page=true&format=png&response_type=json&url=${match[1]}`).then(async (response) => {
+        const { url } = response.data
     	const rest_ss = await axios.get(url, { responseType: 'arraybuffer' })
         await message.sendMessage(Buffer.from(rest_ss.data), MessageType.image, { mimetype: Mimetype.png, caption: `${MLang.by}`, quoted: message.data })
   }).catch (async (err) => {
