12e86171f9908c4b4a4baa2a399f9ee01402cc6e
Ian VanH <70702685+DrkBotBase@users.noreply.github.com>
2022-03-30 12:49:51 -0500

    Update ReplyMessage.js

--- whatsasena/ReplyMessage.js
+++ whatsasena/ReplyMessage.js
@@ -79,15 +79,5 @@ class ReplyMessage extends Base {
     }
 };
 
-const shadowButton = (id, text1, desc1, but = [], options = {}) => {
-      const buttonMessage = {
-        contentText: text1,
-        footerText: desc1,
-        buttons: but,
-        headerType: 1,
-      };
-      message.sendMessage(id, buttonMessage, MessageType.buttonsMessage, options);
-    };
 
-
-module.exports = {ReplyMessage, shadowButton};
+module.exports = ReplyMessage;
